7fc4df73e124e822be751367831ebccf92af6c45
adsric <adam.rickardson@icloud.com>
2019-06-01 13:35:09 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# Setup!
+
+> A shell script to set up a macOS machine for design and development.
+
+Safe to run multiple times on the same machine. It installs, upgrades, or skips
+packages based on what is already installed on the machine.
+
+
+## Install
+
+You can clone the repository wherever you want. (I like to keep it in ~/dev/setup),
+the mac script will automate the process with prompts.
+
+`git clone git@github.com/adsric/setup.git && cd setup`
+
+**Run the script:**
+
+`./mac.sh`
+
+<sub>Just follow the prompts and you’ll be fine. Once the script is done, quit and relaunch Terminal 👌</sub>
+
+**Run the script with a log:**
+
+`./mac.sh 2>&1 | tee ~/setup.log`
+
+:warning: **warning:** I advise against running [this mac script](mac.sh) unless you understand what it’s doing to your computer.
+
+:warning: **warning:** It is highly recommended to run the script regularly to keep your computer up to date.
+
+## The setup
+
+The process will install:
+
+<details>
+<summary>macOS tools:</summary>
+
+* [XCode Command Line Tools](https://developer.apple.com/xcode/downloads/) for developer essentials.
+* [Homebrew](http://brew.sh/) for managing operating system libraries.
+</details>
+
+<details>
+<summary>Unix tools:</summary>
+
+* [Bash](https://www.gnu.org/software/bash/) for a more powerful bash.
+* [Git](https://git-scm.com/) for version control.
+</details>
+
+<details>
+<summary>Package Managers:</summary>
+
+* [NVM](https://github.com/creationix/nvm/) for managing and installing multiple versions of [Node.js](http://nodejs.org/) and [npm](https://www.npmjs.org/)
+</details>
+
+<details>
+<summary>CLI Tools & Utilities:</summary>
+
+* [mackup](https://github.com/lra/mackup) Keep your application settings in sync (OS X/Linux)
+* [mas](https://github.com/mas-cli/mas) Mac App Store command line interface
+</details>
+
+<details>
+<summary>Development</summary>
+
+* [Docker](https://docs.docker.com) Provides a way to run applications securely isolated in a container, packaged with all its dependencies and libraries.
+* [Hyper](https://hyper.is/) for an alternative terminal.
+* [Visual Studio Code](https://code.visualstudio.com/) IDE
+</details>
+
+<details>
+<summary>Design</summary>
+
+* [Sketch](https://www.sketchapp.com/) for design.
+</details>
+
+<details>
+<summary>Communication</summary>
+
+* [Slack](https://slack.com/) where work happens.
+</details>
+
+<details>
+<summary>Browsers</summary>
+
+* [Blisk](https://blisk.io/) for cross-device web development.
+* [Firefox Developer](https://www.mozilla.org/en-GB/firefox/developer/) fast performance, and the development tools you need to build for the open web.
+* [Chrome](https://www.google.com/chrome/browser/desktop/) for fast and free web browsing.
+* [Chrome Canary](https://www.google.com/chrome/canary/) nightly builds of chrome for developers.
+</details>
+
+And set custom [macOS](preferences.sh) preferences.
+
+<sub>See [`install`](install) for the full list of apps that will be installed. Adjust it to your personal taste.</sub>
+
+It should take less than 20 minutes to install (depends on your machine).
+
+
+## Known Issues
+
+Cask does not recognize applications installed outside of Homebrew Cask – in the case that the script fails, you can either remove the application from the install list or uninstall the application causing the failure and try again.
+
+
+## Debugging
+
+Your last setup run will be saved to ~/setup.log (if you chose to run the script with a log). Read through it to see if you can debug the issue yourself. If not, copy the lines where the script failed into a new GitHub Issue. Or, attach the whole log file as an attachment.
---
+++ preferences.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+
+announce "App Store"
+execute "defaults write com.apple.commerce AutoUpdate -bool true" \
+	"Turn on auto-update"
+
+
+announce "Dock"
+execute "defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true" \
+	"Enable spring loading for all Dock items"
+execute "defaults write com.apple.dock mineffect -string 'scale'" \
+	"Change minimize/maximize window effect"
+execute "defaults write com.apple.dock minimize-to-application -bool true" \
+	"Reduce clutter by minimizing windows into their application icons"
+execute "defaults write com.apple.dock mru-spaces -bool false" \
+	"Do not automatically rearrange spaces based on most recent use"
+execute "defaults write com.apple.dock showhidden -bool true" \
+	"Make icons of hidden applications translucent"
+killall "Dock" &> /dev/null
+
+
+announce "Finder"
+execute "defaults write com.apple.finder _FXShowPosixPathInTitle -bool true" \
+	"Use full POSIX path as window title"
+execute "defaults write com.apple.finder WarnOnEmptyTrash -bool false" \
+	"Disable the warning before emptying the Trash"
+execute "defaults write com.apple.finder FXDefaultSearchScope -string 'SCcf'" \
+	"Search the current directory by default"
+execute "defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false" \
+	"Disable warning when changing a file extension"
+execute "defaults write com.apple.finder ShowRecentTags -bool false" \
+	"Do not show recent tags"
+execute "defaults write -g AppleShowAllExtensions -bool true" \
+	"Show all filename extensions"
+killall "Finder" &> /dev/null
+
+
+announce "Safari"
+execute "defaults write com.apple.Safari AutoOpenSafeDownloads -bool false" \
+	"Disable opening 'safe' files automatically"
+execute "defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true" \
+	"Set backspace key to go to the previous page in history"
+execute "defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && \
+		defaults write com.apple.Safari IncludeDevelopMenu -bool true && \
+		defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true" \
+	"Enable the 'Develop' menu and the 'Web Inspector'"
+execute "defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false" \
+	"Set search type to 'Contains' instead of 'Starts With'"
+execute "defaults write com.apple.Safari HomePage -string 'about:blank'" \
+	"Set home page to 'about:blank'"
+execute "defaults write com.apple.Safari IncludeInternalDebugMenu -bool true" \
+	"Enable 'Debug' menu"
+execute "defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true" \
+	"Show the full URL in the address bar"
+execute "defaults write com.apple.Safari SuppressSearchSuggestions -bool true && \
+		defaults write com.apple.Safari UniversalSearchEnabled -bool false" \
+	"Don’t send search queries to Apple"
+execute "defaults write -g WebKitDeveloperExtras -bool true" \
+	"Add a context menu item for showing the 'Web Inspector' in web views"
+killall "Safari" &> /dev/null &
+
+
+announce "Terminal"
+execute "defaults write com.apple.terminal FocusFollowsMouse -string true" \
+	"Make the focus automatically follow the mouse"
+execute "defaults write com.apple.terminal SecureKeyboardEntry -bool true" \
+	"Enable 'Secure Keyboard Entry'"
+execute "defaults write com.apple.Terminal ShowLineMarks -int 0" \
+	"Hide line marks"
+execute "defaults write com.apple.terminal StringEncodings -array 4" \
+	"Only use UTF-8"
+if ! grep -q "pam_tid.so" "/etc/pam.d/sudo"; then
+	execute "sudo sh -c 'echo \"auth sufficient pam_tid.so\" >> /etc/pam.d/sudo'" \
+		"Ensure the Touch ID is used when `sudo` is required"
+fi
+
+
+announce "UI & UX"
+execute "defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true" \
+	"Avoid creating '.DS_Store' files on network volumes"
+execute "defaults write com.apple.CrashReporter UseUNC 1" \
+	"Make crash reports appear as notifications"
+execute "defaults write com.apple.LaunchServices LSQuarantine -bool false" \
+	"Disable 'Are you sure you want to open this application?' dialog"
+execute "defaults write com.apple.print.PrintingPrefs 'Quit When Finished' -bool true" \
+	"Automatically quit the printer app once the print jobs are completed"
+execute "defaults write com.apple.screencapture disable-shadow -bool true" \
+	"Disable shadow in screenshots"
+execute "defaults write com.apple.screencapture location -string '$HOME/Desktop'" \
+	"Save screenshots to the Desktop"
+execute "defaults write com.apple.screencapture show-thumbnail -bool false" \
+	"Do not show thumbnail"
+execute "defaults write com.apple.screencapture type -string 'png'" \
+	"Save screenshots as PNGs"
+execute "defaults write -g NSNavPanelExpandedStateForSaveMode -bool true" \
+	"Expand save panel by default"
+execute "defaults write -g PMPrintingExpandedStateForPrint -bool true" \
+	"Expand print panel by default"
+execute "sudo systemsetup -setrestartfreeze on" \
+	"Restart automatically if the computer freezes"
+killall "SystemUIServer" &> /dev/null
